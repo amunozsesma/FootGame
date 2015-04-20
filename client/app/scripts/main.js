ffa1b2ac94db603273c2b5f43556bea49323a2c3
Mr Alvaro <a.munozsesma@gmail.com>
2015-04-20 23:49:35 +0100

    Deleting client folder as it lives inside the server now

--- client/app/scripts/main.js
+++
@@ -1,4 +0,0 @@
-require(["config", "panel/PanelManager"], function(config, PanelManager) {
-	var panelManager = new PanelManager(document.getElementById("game-area"));
-	panelManager.showPanel(config.initialPanel);
-});
